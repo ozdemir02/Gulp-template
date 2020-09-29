9f4814cea1b514121c30f2735e1dd384e8631910
TolgaAkaMoon <4160141@RTS-365.dk>
2020-09-29 08:17:34 +0200

    add form validation

--- src/javascript/auth.js
+++ src/javascript/auth.js
@@ -2,14 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   let email = document.querySelector(".form__user");
   let pass = document.querySelector(".form__pass");
   let button = document.querySelector(".form__button");
-  console.log(pass.textContent)
 
 //
 
   button.addEventListener("click", function () {
-    if (email.textContent !== "") {
+    if (email.value !== "") {
       document.querySelector(".form__user--error").style.opacity = "0";
-      console.log(hello)
       fetch("https://reqres.in/api/login", {
         method: "POST",
         body: JSON.stringify({
@@ -23,19 +21,16 @@ document.addEventListener("DOMContentLoaded", function () {
       })
         .then((response) => response.json())
         .then((data) => {
-          // localStorage.getItem("token")
           if (data.token !== undefined) {
-            localStorage.setItem("token", data.token);
-          } else {
+            sessionStorage.setItem("token", data.token);
           }
         })
         .catch((err) => console.log(err));
     } else {
         document.querySelector(".form__user--error").innerHTML = "Username cannot be blank"
         document.querySelector(".form__user--error").style.opacity = "1";
-        console.log(email.textContent)
     };
-    if (pass.textContent !== "") {
+    if (pass.value !== "") {
         document.querySelector(".form__pass--error").style.opacity = "0";
     } else {
         document.querySelector(".form__pass--error").innerHTML = "Password cannot be blank"
